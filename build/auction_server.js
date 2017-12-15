09e374209e4734b86c7b0b872a644c091d1ed918
wenxingjie <wen_xingjie@163.com>
2017-12-16 01:50:30 +0800

    修改商品数据，增加轮播图图片的地址

--- build/auction_server.js
+++ build/auction_server.js
@@ -33,17 +33,17 @@ var Comment = (function () {
 }());
 exports.Comment = Comment;
 var products = [
-    new Product(1, "第一个商品", 1.99, 3.5, "这是第一个商品，是我在做面试作品时创建的", ['电子产品', '硬件设备']),
-    new Product(2, "第二个商品", 2.99, 2.5, "这是第二个商品，是我在做面试作品时创建的", ['电子产品', '硬件设备']),
-    new Product(3, "第三个商品", 3.99, 4.5, "这是第三个商品，是我在做面试作品时创建的", ['硬件设备']),
-    new Product(4, "第四个商品", 4.99, 1.5, "这是第四个商品，是我在做面试作品时创建的", ['电子产品', '硬件设备']),
-    new Product(5, "第五个商品", 5.99, 4.5, "这是第五个商品，是我在做面试作品时创建的", ['电子产品']),
-    new Product(6, "第六个商品", 6.99, 2.5, "这是第六个商品，是我在做面试作品时创建的", ['图书']),
+    new Product(1, "第一个商品", 1099, 3.5, "这是第一个商品，是我在做面试作品时创建的", ['玉器', '项链']),
+    new Product(2, "第二个商品", 2099, 4.5, "这是第二个商品，是我在做面试作品时创建的", ['玉器', '项链']),
+    new Product(3, "第三个商品", 3099, 4.5, "这是第三个商品，是我在做面试作品时创建的", ['玉器']),
+    new Product(4, "第四个商品", 4099, 1.5, "这是第四个商品，是我在做面试作品时创建的", ['玉器', '项链']),
+    new Product(5, "第五个商品", 5099, 4.5, "这是第五个商品，是我在做面试作品时创建的", ['项链']),
+    new Product(6, "第六个商品", 6099, 2.5, "这是第六个商品，是我在做面试作品时创建的", ['艺术品']),
 ];
 var comments = [
     new Comment(1, 1, "2017-02-22 22:22:22", "张三", 3, "东西不错"),
-    new Comment(2, 1, "2017-04-22 22:22:22", "李四", 2, "东西一般"),
-    new Comment(3, 1, "2017-07-12 12:22:22", "王五", 4, "东西挺好"),
+    new Comment(2, 1, "2017-04-22 22:22:22", "李四", 4, "东西一般"),
+    new Comment(3, 2, "2017-07-12 12:22:22", "王五", 4, "东西挺好"),
     new Comment(4, 2, "2017-04-02 22:22:22", "赵六", 5, "东西很好"),
 ];
 app.get('/api/products', function (req, res) {
@@ -59,6 +59,9 @@ app.get('/api/products', function (req, res) {
         result = result.filter(function (p) { return p.categories.indexOf(params.category) !== -1; });
     }
     res.json(result);
+});
+app.get('/api/carousel', function (req, res) {
+    res.json(["../../assets/images/carousel1.webp", "../../assets/images/carousel2.webp", "../../assets/images/carousel3.webp"]);
 });
 app.get('/api/product/:id', function (req, res) {
     res.json(products.find(function (product) { return product.id == req.params.id; }));
